19746a242eb8d9bf209a9b086027ed630a47eb0e
Steve Roma <sjroma@hotmail.com>
2017-06-09 12:45:12 -0400

    check in main.js from live coding event in class

--- formBuilder/main.js
+++ formBuilder/main.js
@@ -45,11 +45,11 @@ let formData = [
       },
       {
         "label": "Spanish",
-        "value": "SP"
+        "value": "ES"
       },
       {
         "label": "Chinese",
-        "value": "CH"
+        "value": "CZ"
       },
       {
         "label": "Japanese",
@@ -66,7 +66,7 @@ let formData = [
   },
   {
     "type": "tel",
-    "label": "Mobil Number",
+    "label": "Mobile Number",
     "id": "user-mobile",
     "icon": "fa-mobile-phone",
     "options": []
@@ -87,16 +87,92 @@ formData[0].label // this will give us "First Name"
 // as you can see we access the first element in the array
 // with [0] and then grab the property "label" using the "." character
 
+function createLabel(fieldDef) {
+  let labelNode = document.createElement("label");
+  labelNode.setAttribute("for", fieldDef.id);
+  // labelNode.textContent = fieldDef.label;
+  labelNode.innerText = fieldDef.label;
+
+  return labelNode;
+}
+
+function createSelect(fieldDef) {
+  console.log("select", fieldDef);
+  let divNode = document.createElement("div");
+  let labelNode = createLabel(fieldDef);
+
+  let selectNode = document.createElement("select");
+  selectNode.setAttribute("id", fieldDef.id);
+  selectNode.setAttribute("name", fieldDef.id);
+
+  for (let i = 0; i < fieldDef.options.length; i++) {
+    let optionDef = fieldDef.options[i];
+    let optionNode = document.createElement("option");
+    optionNode.setAttribute("value", optionDef.value);
+    optionNode.innerText = optionDef.label;
+    selectNode.appendChild(optionNode);
+  }
+
+  divNode.appendChild(labelNode);
+  divNode.appendChild(selectNode);
+
+  return divNode;
+}
+
+function createTextarea(fieldDef) {
+  console.log("textarea", fieldDef);
+  let divNode = document.createElement("div");
+  let labelNode = createLabel(fieldDef);
+
+  let textareaNode = document.createElement("textarea");
+  textareaNode.setAttribute("id", fieldDef.id);
+  textareaNode.setAttribute("name", fieldDef.id);
+
+  divNode.appendChild(labelNode);
+  divNode.appendChild(textareaNode);
+
+  return divNode;
+}
+
+function createInput(fieldDef) {
+  console.log("input", fieldDef);
+  let divNode = document.createElement("div");
+  let labelNode = createLabel(fieldDef);
+
+  let inputNode = document.createElement("input");
+  inputNode.setAttribute("type", fieldDef.type);
+  inputNode.setAttribute("id", fieldDef.id);
+  inputNode.setAttribute("name", fieldDef.id);
+
+  divNode.appendChild(labelNode);
+  divNode.appendChild(inputNode);
+
+  return divNode;
+}
+
+function createFieldNode(fieldDef) {
+  let fieldNode;
+  if (fieldDef.type === "select") {
+    fieldNode = createSelect(fieldDef);
+  } else if (fieldDef.type === "textarea") {
+    fieldNode = createTextarea(fieldDef);
+  } else {
+    fieldNode = createInput(fieldDef);    
+  }
+  return fieldNode;
+}
+
+let fieldsNode = document.querySelector(".fields");
+
 // Looping
 // Sample of how to loop over the formData
 for(let i=0; i<formData.length; i++){
-
+  let fieldDef = formData[i];
   // Check your dev tools console for what the items in formData have
-  console.log(formData[i])
-
+  let fieldNode = createFieldNode(fieldDef);
+  fieldsNode.appendChild(fieldNode);
 }
 
 
 // -------- Your Code Goes Here --------
-
 

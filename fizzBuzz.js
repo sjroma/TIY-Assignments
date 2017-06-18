61e402f4920708ed2589d4660b55618cb1c5128a
Steve Roma <sjroma@hotmail.com>
2017-06-17 20:51:47 -0400

    commented for future me

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,3 +1,7 @@
+// Write a program that prints the numbers from 1 to 100. But for multiples of three 
+// print “Fizz” instead of the number and for the multiples of five print “Buzz”. For 
+// numbers which are multiples of both three and five print “FizzBuzz
+
 for (var number = 1; number <= 100; number++)
   if (number % 3 == 0 && number % 5 == 0) 
     console.log("FizzBuzz");
